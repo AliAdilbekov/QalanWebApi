54e1e8f11078de8b1478790aad0896e0a16054b5
Temirlan <temerlan.shodor@gmail.com>
2025-08-07 14:04:25 +0500

    Удалил GitHub Actions

--- src/utils/telegram_alert.py
+++ src/utils/telegram_alert.py
@@ -1,5 +1,5 @@
 import requests
-import os
+from config import CERT_PATH, USE_CUSTOM_CERT
 
 BOT_TOKEN = "7855951281:AAG5WG2QpNlapd0mmrob7F8ZOJxi5l51Mxc"
 CHAT_ID = "-1002372063611"
@@ -7,14 +7,8 @@ CHAT_ID = "-1002372063611"
 def send_telegram_message(text: str):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": text}
-
-    # Отключаем SSL в GitHub Actions
-    verify = False if os.getenv("CI") == "true" else True
-
-    try:
-        response = requests.post(url, json=payload, verify=verify)
-        if response.status_code != 200:
-            print(f"[ERROR] Telegram response: {response.text}")
-        return response
-    except Exception as e:
-        print(f"[WARNING] Telegram send failed: {e}")
+    verify = CERT_PATH if USE_CUSTOM_CERT else False
+    response = requests.post(url, json=payload, verify=verify)
+    if response.status_code != 200:
+        print(f"[ERROR] Telegram response: {response.text}")
+    return response
