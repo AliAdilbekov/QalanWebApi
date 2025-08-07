34415619bda3cd6ccd46fd7456df7498c750088b
Temirlan <temerlan.shodor@gmail.com>
2025-08-07 17:54:59 +0500

    parametrization base urls config.py

--- config.py
+++ config.py
@@ -1,10 +1,24 @@
 import os
 
+# если окружение не указывать, используется дефолт — preprod
+ENV = os.getenv("ENV", "preprod")
 
-BASE_URL = "https://preprod.qalan.kz/api"
+BASE_URLS = {
+    # "prod": "https://qalan.kz/api",
+    "test": "https://test.qalan.kz/api",
+    "preprod": "https://preprod.qalan.kz/api",
+}
+
+CERTS = {
+    # "prod": "certs/qalan_prod.crt",
+    "test": "certs/qalan_test.crt",
+    "preprod": "certs/qalan_preprod.crt",
+}
+
+BASE_URL = BASE_URLS[ENV]
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CERT_PATH = os.path.join(BASE_DIR, "certs", "qalan_preprod.crt")
+CERT_PATH = os.path.join(BASE_DIR, CERTS[ENV])
 
-# Использовать SSL-проверку или нет
+# Отключаем кастомные сертификаты (временно)
 USE_CUSTOM_CERT = False
